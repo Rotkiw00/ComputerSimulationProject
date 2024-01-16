00b742fa5c5a64cb27e5d08057ccb30bd60f957e
Roktiw00 <wkalaga73@gmail.com>
2024-01-16 20:42:34 +0100

    Adjusted functionality for blazor webview components

--- Project/ElectionSimulatorWPF/SummaryWindow.xaml.cs
+++ Project/ElectionSimulatorWPF/SummaryWindow.xaml.cs
@@ -1,27 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ElectionSimulatorWPF
 {
-    /// <summary>
-    /// Interaction logic for SummaryWindow.xaml
-    /// </summary>
-    public partial class SummaryWindow : Window
+	/// <summary>
+	/// Interaction logic for SummaryWindow.xaml
+	/// </summary>
+	public partial class SummaryWindow : Window
     {
         public SummaryWindow()
         {
-            InitializeComponent();
+			var serviceCollection = new ServiceCollection();
+			serviceCollection.AddWpfBlazorWebView();
+			Resources.Add("services", serviceCollection.BuildServiceProvider());
+
+			InitializeComponent();
         }
 
         private void btnBackToMainWindow_Click(object sender, RoutedEventArgs e)
