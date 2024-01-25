a92d98c910b142d65c36322c12c102f16761b415
Roktiw00 <wkalaga73@gmail.com>
2024-01-25 13:43:39 +0100

    Refactored code in summary window and razor component - concept phase

--- Project/ElectionSimulatorWPF/SimulationStats.razor.cs
+++ Project/ElectionSimulatorWPF/SimulationStats.razor.cs
@@ -3,138 +3,68 @@ using PSC.Blazor.Components.Chartjs.Models.Doughnut;
 using PSC.Blazor.Components.Chartjs;
 using PSC.Blazor.Components.Chartjs.Models.Bar;
 
-namespace ElectionSimulatorWPF
+namespace ElectionSimulatorWPF;
+
+public partial class SimulationStats
 {
-	public partial class SimulationStats
+	// wykresy z tego nugeta muszą być obiektami
+	// TODO: trzeba ogarnąć, żeby one nie były tak luźno
+	// w kodzie
+	private BarChartConfig _barChartConfig1;
+	private Chart _barChart1;
+
+	private DoughnutChartConfig _doughnutChartConfig2;
+	private Chart _doughnutChart2;
+
+	private DoughnutChartConfig _doughnutChartConfig3;
+	private Chart _doughnutChart3;
+
+	// wykresy NIE mogą być wyświetlane przy inicjalizacji WebView
+	// TODO: Dorzucić jakąś flagę, wskazującą na otrzymanie danych i możliwość wyświetlenia wykresów
+
+	// Zaciągane z WPF dane
+	// trzeba je obrobić i przypisać do congifa danego wykresu
+	private Results _simulationResults;
+	public Results SimulationResults
 	{
-		// Zaciągane z WPF dane
-		// trzeba je obrobić i przypisać do congifa danego wykresu
-		private Results _simulationResults;
-		public Results SimulationResults
+		get => _simulationResults; 
+		set
 		{
-			get => _simulationResults; set
-			{
-				_simulationResults = value;
-				StateHasChanged();
-			}
+			_simulationResults = value;
+			StateHasChanged();
 		}
+	}
 
-		private bool _isDataLoaded;
-		public bool IsDataLoaded
+	private bool _isDataLoaded;
+	public bool IsDataLoaded
+	{
+		get => _isDataLoaded; 
+		set
 		{
-			get => _isDataLoaded; set
-			{
-				_isDataLoaded = value;
-				StateHasChanged();
-			}
+			_isDataLoaded = value;
+			StateHasChanged();
 		}
+	}		
 
-		// wykresy z tego nugeta muszą być obiektami
-		// TODO: trzeba ogarnąć, żeby one nie były tak luźno
-		// w kodzie
-		private BarChartConfig _barChartConfig1;
-		private Chart _barChart1;
-
-		private DoughnutChartConfig _doughnutChartConfig2;
-		private Chart _doughnutChart2;
-
-		private DoughnutChartConfig _doughnutChartConfig3;
-		private Chart _doughnutChart3;
-
-		// wykresy NIE mogą być wyświetlane przy inicjalizacji WebView
-		// TODO: Dorzucić jakąś flagę, wskazującą na otrzymanie danych i możliwość wyświetlenia wykresów
-		protected override Task OnInitializedAsync()
+	protected IEnumerable<decimal?> GetElectionResults()
+	{
+		foreach (var dataItem in SimulationResults.ResultsSejm.WynikiProcentowe)
 		{
-			AppState.SimulationStatsComponent = this;
+			yield return (decimal)dataItem;
+		}
+	}
+	// napisać metodę, która od razu przy deserializacji konwertuje double token na decimal
+	protected IEnumerable<string> GetElectionPartiesNames()
+	{
+		foreach (var partyName in SimulationResults.ResultsSejm.EtykietyPartii)
+		{
+			yield return partyName;
+		}
+	}
 
-			_barChartConfig1 = new BarChartConfig()
-			{
-				Options = new Options()
-				{
-					Plugins = new Plugins()
-					{
-						Legend = new Legend()
-						{
-							Align = Align.Center,
-							Display = false,
-							Position = LegendPosition.Right
-						}
-					},
-				}
-			};
-
-			_barChartConfig1.Data.Labels = ["Koalicja Obywatelska",
-				"Zjednoczona Prawica",
-				"Trzecia Droga",
-				"Nowa Lewica",
-				"Konfederacja",
-				"Bezpartyjni Samorządowcy"];
-			List<decimal?> data1 = [100, 200, 400];
-
-			_barChartConfig1.Data.Datasets.Add(new BarDataset()
-			{
-				Data = data1,
-				BorderWidth = 1
-			});
-
-			// ========================================================
-
-			_doughnutChartConfig2 = new DoughnutChartConfig()
-			{
-				Options = new Options()
-				{
-					Responsive = true,
-					MaintainAspectRatio = false,
-					Plugins = new Plugins()
-					{
-						Legend = new Legend()
-						{
-							Display = true,
-							Position = LegendPosition.Left
-						}
-					}
-				}
-			};
-
-			_doughnutChartConfig2.Data.Labels = ["January", "February", "March"];
-			List<decimal?> data2 = [100, 200, 400];
-
-			_doughnutChartConfig2.Data.Datasets.Add(new DoughnutDataset()
-			{
-				Data = data2,
-				BackgroundColor = ["yellow", "red", "blue"],
-				HoverOffset = 4
-			});
-
-			// ========================================================
-
-			_doughnutChartConfig3 = new DoughnutChartConfig()
-			{
-				Options = new Options()
-				{
-					Responsive = true,
-					MaintainAspectRatio = false,
-					Plugins = new Plugins()
-					{
-						Legend = new Legend()
-						{
-							Display = true,
-							Position = LegendPosition.Left
-						}
-					}
-				}
-			};
-
-			_doughnutChartConfig3.Data.Labels = ["January", "February", "March"];
-			List<decimal?> data = [100, 200, 400];
-
-			_doughnutChartConfig3.Data.Datasets.Add(new DoughnutDataset()
-			{
-				Data = data,
-				BackgroundColor = ["yellow", "red", "blue"],
-				HoverOffset = 4
-			});
-			return Task.CompletedTask;
-		}		
+	protected override Task OnInitializedAsync()
+	{
+		WndwState.SimulationStatsComponent = this;
+		return Task.CompletedTask;
 	}
 }
--- Project/ElectionSimulatorWPF/SummaryWindow.xaml.cs
+++ Project/ElectionSimulatorWPF/SummaryWindow.xaml.cs
@@ -1,123 +1,95 @@
 ﻿using System.IO;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 using System.Windows;
 using Blazored.Modal;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace ElectionSimulatorWPF
+namespace ElectionSimulatorWPF;
+
+/// <summary>
+/// Interaction logic for SummaryWindow.xaml
+/// </summary>
+public partial class SummaryWindow : Window
 {
-	/// <summary>
-	/// Interaction logic for SummaryWindow.xaml
-	/// </summary>
-	public partial class SummaryWindow : Window
+	public SummaryWindow()
 	{
-		public SummaryWindow()
-		{
-			var serviceCollection = new ServiceCollection();
-			serviceCollection.AddWpfBlazorWebView();
-			serviceCollection.AddBlazoredModal();
-			Resources.Add("services", serviceCollection.BuildServiceProvider());
+		var serviceCollection = new ServiceCollection();
+		serviceCollection.AddWpfBlazorWebView();
+		serviceCollection.AddBlazoredModal();
+		Resources.Add("services", serviceCollection.BuildServiceProvider());
 
-			InitializeComponent();
-			AppState.SummaryWindow = this;
-		}
+		InitializeComponent();
+		WndwState.SummaryWindow = this;
+	}
 
-		private void btnBackToMainWindow_Click(object sender, RoutedEventArgs e)
-		{
-			var objectMainWindow = new MainWindow();
-			this.Visibility = Visibility.Collapsed;
-			objectMainWindow.Show();
-		}
+	private void btnBackToMainWindow_Click(object sender, RoutedEventArgs e)
+	{
+		var objectMainWindow = new MainWindow();
+		this.Visibility = Visibility.Collapsed;
+		objectMainWindow.Show();
+	}
 
-		private void btnLoadAndShowRezults_Click(object sender, RoutedEventArgs e)
+	private void btnLoadAndShowRezults_Click(object sender, RoutedEventArgs e)
+	{
+		var dialog = new Microsoft.Win32.OpenFileDialog()
 		{
-			var dialog = new Microsoft.Win32.OpenFileDialog()
+			DefaultExt = ".json",
+			Filter = "JSON (.json)|*.json|All files|*.*"
+		};
+
+		bool? isOpen = dialog.ShowDialog();
+		if (isOpen is true)
+		{
+			string rezultsFilePath = dialog.FileName;
+			if (Path.GetExtension(rezultsFilePath) is not ".json")
 			{
-				DefaultExt = ".json",
-				Filter = "JSON (.json)|*.json|All files|*.*"
-			};
-
-			bool? isOpen = dialog.ShowDialog();
-			if (isOpen is true)
-			{
-				string rezultsFilePath = dialog.FileName;
-				if (Path.GetExtension(rezultsFilePath) is not ".json")
-				{
-					MessageBox.Show("Błędny format pliku. Nie można wyświetlić wyniku", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-				}
-				else
-				{
-					RetrieveDataFromJson(rezultsFilePath);
-				}
+				MessageBox.Show("Błędny format pliku. Nie można wyświetlić wyniku", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
 			}
 			else
 			{
-				MessageBox.Show("Nie wybrano pliku. Nie można wyświetlić wyniku", "Uwaga", MessageBoxButton.OK, MessageBoxImage.Warning);
+				RetrieveDataFromJson(rezultsFilePath);
 			}
 		}
-
-		// metoda zczytująca jsona. Powinna zwracać dane do Blazora i tam się nimi już zajmować
-		private static void RetrieveDataFromJson(string pathData)
+		else
 		{
-			string json = File.ReadAllText(pathData);
-			var results = JsonSerializer.Deserialize<Results>(json);
-			// przekazać w metodzie
-			AppState.SimulationStatsComponent.SimulationResults = results;
-			AppState.SimulationStatsComponent.IsDataLoaded = true;
-		}
-
-		// jakis badziew
-		public void ShowMessageBox(string Message)
-		{
-			MessageBox.Show(Message);
+			MessageBox.Show("Nie wybrano pliku. Nie można wyświetlić wyniku", "Uwaga", MessageBoxButton.OK, MessageBoxImage.Warning);
 		}
 	}
 
-	// referencja 'do' i 'z' WPF względem Blazora
-	public static class AppState
-	{ 
-		public static SummaryWindow SummaryWindow { get; set; } 
-		public static SimulationStats SimulationStatsComponent { get; set; }
-	}
-
-	// testowo sprawdzić ładowanie JSONa. Bazowe wyniki. TODO: do zmiany
-	// i wywalić do jakiegoś folderu typu model albo inne podejście
-	public class PoparciePartii
+	// metoda zczytująca jsona. Powinna zwracać dane do Blazora i tam się nimi już zajmować
+	private static void RetrieveDataFromJson(string pathData)
 	{
-		[JsonPropertyName("Koalicja Obywatelska")]
-		public decimal KoalicjaObywatelska { get; set; }
-
-		[JsonPropertyName("Zjednoczona Prawica")]
-		public decimal ZjednoczonaPrawica { get; set; }
-
-		[JsonPropertyName("Trzecia Droga")]
-		public decimal TrzeciaDroga { get; set; }
-
-		[JsonPropertyName("Nowa Lewica")]
-		public decimal NowaLewica { get; set; }
-
-		[JsonPropertyName("Konfederacja")]
-		public decimal Konfederacja { get; set; }
-
-		[JsonPropertyName("Bezpartyjni Samorządowcy")]
-		public decimal BezpartyjniSamorzdowcy { get; set; }
+		string json = File.ReadAllText(pathData);
+		var results = JsonSerializer.Deserialize<Results>(json);
+		// przekazać w metodzie
+		WndwState.SimulationStatsComponent.SimulationResults = results;
+		WndwState.SimulationStatsComponent.IsDataLoaded = true;
 	}
+}
 
-	public class ResultsSejm
-	{
-		public List<PoparciePartii> PoparciePartii { get; set; }
-		public List<object> RozkladMandatow { get; set; }
-	}
+// referencja 'do' i 'z' WPF względem Blazora
+public static class WndwState
+{ 
+	public static SummaryWindow SummaryWindow { get; set; } 
+	public static SimulationStats SimulationStatsComponent { get; set; }
+}
 
-	public class ResultsSenat
-	{
-		public List<object> RozkladMandatow { get; set; }
-	}
+// testowo sprawdzić ładowanie JSONa. Bazowe wyniki. TODO: do zmiany
+// i wywalić do jakiegoś folderu typu model albo inne podejście
+public class ResultsSejm
+{
+	public List<string> EtykietyPartii { get; set; }
+	public List<double> WynikiProcentowe { get; set; }
+	public List<object> RozkladMandatow { get; set; }
+}
 
-	public class Results
-	{
-		public ResultsSejm ResultsSejm { get; set; }
-		public ResultsSenat ResultsSenat { get; set; }
-	}
+public class ResultsSenat
+{
+	public List<object> RozkladMandatow { get; set; }
+}
+
+public class Results
+{
+	public ResultsSejm ResultsSejm { get; set; }
+	public ResultsSenat ResultsSenat { get; set; }
 }
