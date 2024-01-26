8bd28877eca5525fb91b3d889bac5e7cfdcad1ad
Roktiw00 <wkalaga73@gmail.com>
2024-01-26 01:29:36 +0100

    Added new components and modified existing

--- Project/ElectionSimulatorWPF/ConfigSimulationFormWindow.xaml.cs
+++ Project/ElectionSimulatorWPF/ConfigSimulationFormWindow.xaml.cs
@@ -19,6 +19,9 @@ namespace ElectionSimulatorWPF
 	/// </summary>
 	public partial class ConfigSimulationFormWindow : Window
 	{
+		private int gridsPartyCreator;
+		private List<TextBox> dynamicTextBoxes = [];
+
 		public ConfigSimulationFormWindow()
 		{
 			InitializeComponent();
@@ -34,6 +37,83 @@ namespace ElectionSimulatorWPF
 		private void btnSaveConfigForm_Click(object sender, RoutedEventArgs e)
 		{
 			MessageBox.Show("Zapisywanie konfiguracji do symulacji", "Info");
+
+			var mapSimulationWindow = new MapSimulationWindow();
+			this.Visibility = Visibility.Collapsed;
+			mapSimulationWindow.Show();
+
+			// Zapisywanie kofiguracji ??
+		}
+		
+		private void btnCreatePartiesControl_Click(object sender, RoutedEventArgs e)
+		{
+			btnCreatePartiesControl.IsEnabled = false;
+
+			if (!String.IsNullOrEmpty(txtParties.Text))
+			{
+				if (int.TryParse(txtParties.Text, out gridsPartyCreator))
+				{
+					for (int i = 0; i < gridsPartyCreator; i++)
+					{
+						CreatePartyControlForms();
+					}
+				}
+			}
+		}
+
+		private void CreatePartyControlForms()
+		{
+			Grid grid = new()
+			{
+				Height = 50
+			};
+
+			ColumnDefinition column1 = new()
+			{
+				Width = new GridLength(1, GridUnitType.Star)
+			};
+			ColumnDefinition column2 = new()
+			{
+				Width = new GridLength(1, GridUnitType.Star)
+			};
+
+			grid.ColumnDefinitions.Add(column1);
+			grid.ColumnDefinitions.Add(column2);
+
+			Label label = new()
+			{
+				Content = "Ile partii politycznych do symulacji ?",
+				VerticalAlignment = VerticalAlignment.Center,
+				FontSize = 25,
+				Margin = new Thickness(10, 0, 0, 0),
+				Height = 44
+			};
+			Grid.SetColumn(label, 0);
+			grid.Children.Add(label);
+
+			TextBox textBox = new()
+			{
+				Name = "txtParty",
+				VerticalAlignment = VerticalAlignment.Center,
+				FontSize = 23,
+				Margin = new Thickness(522, 0, 392, 0)
+			};
+			Grid.SetColumnSpan(textBox, 2);
+			grid.Children.Add(textBox);
+			dynamicTextBoxes.Add(textBox);
+
+			Button button = new()
+			{
+				Name = "btnCreatePartiesControl",
+				Content = "Zatwierdź",
+				Margin = new Thickness(216, 9, 81, 10),
+				FontSize = 20
+			};
+			button.Click += btnCreatePartiesControl_Click;
+			Grid.SetColumn(button, 1);
+			grid.Children.Add(button);
+
+			stackPanelView.Children.Add(grid);
 		}
 	}
 }
---
+++ Project/ElectionSimulatorWPF/MapSimulationWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ElectionSimulatorWPF
+{
+	/// <summary>
+	/// Interaction logic for MapSimulationWindow.xaml
+	/// </summary>
+	public partial class MapSimulationWindow : Window
+	{
+		public MapSimulationWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
